5a29eaee8068b29aa9ba8b2a911a9c6e9d867047
flexibleBrain <yxj1141@sina.com>
2016-11-29 22:57:56 +0800

    add HttpFileServer

---
+++ src/main/java/com/smarts/netty/HttpFileServer.java
@@ -0,0 +1,54 @@
+package com.smarts.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * Created by yangxuejun
+ * on 16-11-29 下午10:29
+ */
+public class HttpFileServer {
+    private static final String DEFAULT_URL="/home/yxj/IdeaProjects/netty/";
+
+    public  void run(final int port ,final String url){
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("http-decoder",new HttpRequestDecoder());
+                            ch.pipeline().addLast("http-aggregator",new HttpObjectAggregator(65536));
+                            ch.pipeline().addLast("http-encoder",new HttpResponseEncoder());
+                            ch.pipeline().addLast("http-chunked",new ChunkedWriteHandler());
+                            ch.pipeline().addLast("fileServerhander",new HttpFileServerHandler(url));
+                        }
+                    });
+            ChannelFuture future = b.bind("127.0.0.1",port);
+            System.out.println("Http 文件目录服务器启动，网址是：http://localhost:"+port+url);
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+    public static void main(String[] args) {
+        int port = 8080;
+        new HttpFileServer().run(port, DEFAULT_URL);
+
+    }
+}
